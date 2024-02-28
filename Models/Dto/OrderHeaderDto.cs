e540be10f5fd266a7719b3129b4bc551c8555359
PaoloPci <paolopci@yahoo.it>
2024-02-28 10:42:07 +0100

    008 Constants in Order API

--- Models/Dto/OrderHeaderDto.cs
+++ Models/Dto/OrderHeaderDto.cs
@@ -19,7 +19,7 @@
         public string? PaymentIntentId { get; set; }
         public string? StripeSessionId { get; set; }
 
-        public IEnumerable<OrderDetails> OrderDetails { get; set; }
+        public IEnumerable<OrderDetailsDto> OrderDetails { get; set; }
     }
 }
  
