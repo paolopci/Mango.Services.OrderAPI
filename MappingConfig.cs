fcb2f365281f35ba98e96c9c962b17b786110c5f
PaoloPci <paolopci@yahoo.it>
2024-02-28 10:29:26 +0100

    007 Mapping Config for Order API

--- MappingConfig.cs
+++ MappingConfig.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Mango.Services.OrderAPI.Models;
+using Mango.Services.OrderAPI.Models.Dto;
 
 
 namespace Mango.Services.OrderAPI
@@ -10,9 +12,31 @@ namespace Mango.Services.OrderAPI
         {
             var mappingConfig = new MapperConfiguration(config =>
             {
-                // perchè funzioni assicurati che il nome delle proprietà inSource e Target siano le stesse
-                //config.CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
-                //config.CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+                // Nota: qui abbiamo il problema che nella tabella OrderHeaderDto abbiamo il campo OrderTotal mentre nella tabella 
+                // CartHeaderDto abbiamo il campo CarTotal che è lo stesso ma con nomi diversi quindi 
+                // devo segnarlo con AutoMapper
+                config.CreateMap<OrderHeaderDto, CartHeaderDto>()
+                    .ForMember(dest => dest.CartTotal,
+                        u =>
+                            u.MapFrom(src => src.OrderTotal)).ReverseMap();
+
+
+
+                // Nota: qui abbiamo il problema che nella tabella OrderDetailsDto ho il campo ProductName 'string' mentre nella tabella
+                // CartDetailsDto abbiamo l'oggetto ProductDto? Product quindi devo mappare la cosa con AutoMapper
+                // stessa cosa con il campo Price sempre nella tabella CartDetailsDto che lo devo mappare 
+
+                config.CreateMap<CartDetailsDto, OrderDetailsDto>()
+                    .ForMember(dest => dest.ProductName, u
+                    => u.MapFrom(src => src.Product.Name))
+                    .ForMember(dest => dest.Price, u
+                    => u.MapFrom(src => src.Product.Price));
+
+
+                // qui mappo le tabelle del DB con quello del Dto
+                config.CreateMap<OrderDetailsDto, CartDetailsDto>();
+                config.CreateMap<OrderHeader, OrderHeaderDto>().ReverseMap();
+                config.CreateMap<OrderDetailsDto, OrderDetails>().ReverseMap();
             });
             return mappingConfig;
         }
