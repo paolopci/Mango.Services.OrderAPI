46cfa3f8250b328a46bd9868365da574d7023bd5
PaoloPci <paolopci@yahoo.it>
2024-02-28 15:13:00 +0100

    002 Order Confirmation Page

---
+++ Models/Dto/OrderDetailsDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Mango.Services.OrderAPI.Models.Dto
+{
+    public class OrderDetailsDto
+    {
+       
+        public int OrderDetailsId { get; set; }
+        public int OrderHeaderId { get; set; }
+       // public OrderHeader? OrderHeader { get; set; }    per evitare loop infinito!!!!!
+        public int ProductId { get; set; }
+        public ProductDto? Product { get; set; }
+        public int Count { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+    }
+}
