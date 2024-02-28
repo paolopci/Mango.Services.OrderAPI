d02c9ef92b8ee5722a5f7c47bbd387b9cfc042da
PaoloPci <paolopci@yahoo.it>
2024-02-28 15:28:04 +0100

    add new files

---
+++ Controllers/OrderAPIController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Mango.MessageBus;
+using Mango.Services.OrderAPI.Data;
+using Mango.Services.OrderAPI.Models.Dto;
+using Mango.Services.OrderAPI.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.OrderAPI.Controllers
+{
+    [Route("api/order")]
+    [ApiController]
+    public class OrderAPIController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private IMapper _mapper;
+        private readonly AppDbContext _db;
+        private IProductService _productService;
+        private readonly IMessageBus _messageBus;
+        private readonly IConfiguration _configuration;
+        public OrderAPIController(AppDbContext db,
+            IProductService productService, IMapper mapper, IConfiguration configuration
+            , IMessageBus messageBus)
+        {
+            _db = db;
+            _messageBus = messageBus;
+            this._response = new ResponseDto();
+            _productService = productService;
+            _mapper = mapper;
+            _configuration = configuration;
+        }
+    }
+}
